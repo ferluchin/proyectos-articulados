bc4af9ac1bc31066a0bb78e853405cfb5d5405dd
ferluchin <lgrandab@gmail.com>
2022-09-04 21:31:28 -0500

    Primer Commit Template
    el template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyectos-articulados';
+
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+
+  thirdFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+
+  fourthFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+
+  fifthFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+
+  sixthFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+
+  seventhFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+
+  eighthFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+
+  ninthFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+
+
+
+  isLinear = false;
+
+  constructor(private _formBuilder: FormBuilder) { }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatStepperModule } from '@angular/material/stepper';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
+    HttpClientModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
